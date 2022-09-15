93d7bfb36e4e76ff0478dff5b3b68754549d44e3
Roberto Sepulveda <67658859+Robie94@users.noreply.github.com>
2022-09-14 20:50:21 -0500

    Add files via upload

---
+++ PROC9_P5Juego/sketch.js
@@ -0,0 +1,31 @@
+var box ;
+function move_sprite(){
+  if(keyIsDown(RIGHT_ARROW)) {
+    box.position.x=box.position.x+5
+  }
+  if(keyIsDown(LEFT_ARROW)) {
+    box.position.x=box.position.x-5
+  }
+
+  if(keyIsDown(UP_ARROW)) {
+    box.position.y=box.position.y-5
+  }
+  if(keyIsDown(DOWN_ARROW)) {
+    box.position.y=box.position.y+5
+  }
+}
+function setup() {
+  createCanvas(1000,600);
+  box=createSprite(200,200,30,30);
+}
+
+function draw() 
+{
+  background(30);
+  move_sprite();
+  drawSprites();
+}
+
+
+
+
